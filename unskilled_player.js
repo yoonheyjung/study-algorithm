bdf1686d6a5c6f3928598b2f3ca5530bf9ee83a4
혜정 <heyjung@bbangyanetworks.com>
2022-03-28 02:40:50 +0900

    완주하지못한 선수

---
+++ unskilled_player.js
@@ -0,0 +1,20 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      console.log(`완주하지 못 한 사람은 ${participant[i]} 입니다`);
+      return participant[i];
+    }
+  }
+
+  return "hi";
+}
+
+solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
+solution(
+  ["marina", "josipa", "nikola", "vinko", "filipa"],
+  ["josipa", "filipa", "marina", "nikola"]
+);
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
